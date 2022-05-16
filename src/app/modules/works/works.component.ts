e0bfc918877d3b3f1bcba4476b9f0f2cae1f4ee3
Szilárd <szilard.salamon@trendency.hu>
2022-05-16 16:42:47 +0200

    portfolio dummy

--- src/app/modules/works/works.component.ts
+++ src/app/modules/works/works.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './works.component.html',
   styleUrls: ['./works.component.scss'],
 })
-export class WorksComponent {}
+export class WorksComponent {
+  works = [
+    {
+      image: 'https://picsum.photos/310/266',
+      company: 'ARH Ikjjp;o.',
+      desc: 'Ez egy hosszabb duma is lehe, k,otne, de mi a francos mennykonek kene hogy az legyen',
+    },
+    {
+      image: 'https://picsum.photos/310/266',
+      company: 'ARH Inc.',
+      desc: 'Ez egy hosszabb duma is lehetne, de mi a francos mennykonek kene hogy az legyen',
+    },
+    {
+      image: 'https://picsum.photos/310/266',
+      company: 'ARH Inc.',
+      desc: 'Ez egy hosszabb duma is lehetne, de mi a francos mennykonek kene hogy az legyen',
+    },
+    {
+      image: 'https://picsum.photos/310/266',
+      company: 'ARH Inc.',
+      desc: 'Ez egy hosszabb duma is lehetne, de mi a francos mennykonek kene hogy az legyen',
+    },
+  ];
+}
