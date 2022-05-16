e0c8c8bd1c7b6cb70d3a398095fd5262152cc476
Szilárd <szilard.salamon@trendency.hu>
2022-05-16 14:54:14 +0200

    portfolio begin

--- src/app/modules/works/works.component.ts
+++ src/app/modules/works/works.component.ts
@@ -1,25 +1,8 @@
 import { Component } from '@angular/core';
-import { ProjectService } from '../shared/project.service';
-import { Project } from '../shared/project-interface';
-import { Router } from '@angular/router';
-import { TruncatePipe } from '../shared/trunc.pipe';
 
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
   styleUrls: ['./works.component.scss'],
-  pipes: [TruncatePipe],
 })
-export class WorksComponent implements OnInit {
-  rojects: Project[];
-
-  constructor(public _projectService: ProjectService, public router: Router) {}
-
-  chooseProject(project) {
-    this.router.navigate(['detail', project.id]);
-  }
-
-  ngOnInit() {
-    this._projectService.getProjects().then((projects) => (this.projects = projects));
-  }
-}
+export class WorksComponent {}
